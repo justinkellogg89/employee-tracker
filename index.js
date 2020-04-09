77cf51bc14189d13e38cb970ef7dfa339240d84f
Justin <jlkellogg18@outlook.com>
2020-04-08 20:48:56 -0700

    required ctable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
-const cTable = require("console.table");
 const mysql = require("mysql");
-const questions = require("./questions")
+const questions = require("./questions");
+require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -9,55 +9,63 @@ const connection = mysql.createConnection({
   user: "root",
   // Your password
   password: "",
-  database: "employees_db"
+  database: "employees_db",
 });
 
-connection.connect(function(err){
-    if (err) throw err;
-    runActions();
+connection.connect(function (err) {
+  if (err) throw err;
+  runActions();
 });
 
 function runActions() {
-    inquirer.prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role"]
-    }).then(function(answer){
-        switch(answer.action){
-            case "Add Department":
-                addDepartment();
-                break;
-
-            case "Add Role":
-                addRole();
-                break;
-
-            case "Add Employee":
-                addEmployee();
-                break;
-
-            case "View Departments":
-                viewDepartments();
-                break;
-
-            case "View Roles":
-                viewRoles();
-                break;
-
-            case "View Employees":
-                viewEmployees();
-                break;
-
-            case "Update Employee Role":
-                updateEmployeeRole();
-                break;
-        }
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Update Employee Role",
+      ],
     })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "View Departments":
+          viewDepartments();
+          break;
+
+        case "View Roles":
+          viewRoles();
+          break;
+
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+      }
+    });
 }
 
-function addDepartment(){
-    inquirer.prompt(questions.addDepartment);
+function addDepartment() {
+  inquirer.prompt(questions.addDepartment);
 }
-
-
